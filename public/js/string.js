4a01ac83969b8ce9828b08b3b24b8a01b2104cd0
Yujin Chung <yujinchung863@gmail.com>
2019-01-05 06:27:05 +0000

    [#11] Added enums

--- public/js/string.js
+++ public/js/string.js
@@ -3,9 +3,9 @@
 /*******************************************************
 ** Database String *************************************
 *******************************************************/
-var test_database = 'mongodb://localhost/test_databas';
-var dev_database = 'mongodb://localhost/dev_database';
-var main_database = 'mongodb://localhost/main_database';
+const test_database = 'mongodb://localhost/test_databas';
+const dev_database = 'mongodb://localhost/dev_database';
+const main_database = 'mongodb://localhost/main_database';
 
 // set this database to dev_database or main_database
 var database = test_database;
@@ -13,4 +13,22 @@ var database = test_database;
 /*********************************************************
 ** Error Message ****************************************
 ********************************************************/
-var error_404 = 'Error occured 404';
+const error_404 = 'Error occured 404';
+
+
+/*********************************************************
+** Enum values ****************************************
+********************************************************/
+const suit = {
+    SPADE : 0,
+    DIAMOND : 1,
+    HEART : 2,
+    CLOVER : 3,
+    NONE : 4
+};
+const job = {
+    NONE : 0,
+    JOKER : 1,
+    JOKER_CALL : 2,
+    MIGHTY : 3
+};
