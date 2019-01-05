dd4e447ed1bd23ffdd605e450a7c851318512dfe
Yujin Chung <yujinchung863@gmail.com>
2019-01-05 06:16:05 +0000

    [#11] Set public/js/string.js

---
+++ public/js/string.js
@@ -0,0 +1,16 @@
+// string.js : for static string values such as database...
+
+/*******************************************************
+** Database String *************************************
+*******************************************************/
+var test_database = 'mongodb://localhost/test_databas';
+var dev_database = 'mongodb://localhost/dev_database';
+var main_database = 'mongodb://localhost/main_database';
+
+// set this database to dev_database or main_database
+var database = test_database;
+
+/*********************************************************
+** Error Message ****************************************
+********************************************************/
+var error_404 = 'Error occured 404';
---
+++ routes/index.js
@@ -0,0 +1,52 @@
+// routes/index.js
+
+module.exports = function(app, Book)
+{
+    // GET ALL BOOKS
+    app.get('/api/books', function(req,res){
+        Book.find(function(err, books){
+        if(err) return res.status(500).send({error: 'database failure'});
+            res.json(books);
+        })
+    });
+
+    // GET SINGLE BOOK
+    app.get('/api/books/:book_id', function(req, res){
+        res.end();
+    });
+
+    // GET BOOK BY AUTHOR
+    app.get('/api/books/author/:author', function(req, res){
+        res.end();
+    });
+
+    // CREATE BOOK
+    app.post('/api/books', function(req, res){
+        var book = new Book();
+        book.title = req.body.name;
+        book.author = req.body.author;
+        book.published_date = new Date(req.body.published_date);
+
+        book.save(function(err){
+            if(err){
+                console.error(err);
+                res.json({result: 0});
+                return;
+            }
+
+            res.json({result: 1});
+
+        });
+    });
+
+    // UPDATE THE BOOK
+    app.put('/api/books/:book_id', function(req, res){
+        res.end();
+    });
+
+    // DELETE BOOK
+    app.delete('/api/books/:book_id', function(req, res){
+        res.end();
+    });
+
+}
